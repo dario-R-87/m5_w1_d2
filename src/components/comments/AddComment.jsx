aa70a3bcfada2c78e802210763a2d36c7c491c72
Dario Russo <dariorusso987@gmail.com>
2024-02-17 17:09:46 +0100

    fix post

--- src/components/comments/AddComment.jsx
+++ src/components/comments/AddComment.jsx
@@ -1,29 +1,148 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useReducer } from "react";
 
 const AddComment = ({ onAdd }) => {
-    return (
-        <Form onSubmit={onAdd}>
-            <h3 className="text-center">Add new comment</h3>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Comment</Form.Label>
-                <Form.Control type="email" placeholder="Enter comment" />
-                <Form.Text className="text-muted">
-                </Form.Text>
-            </Form.Group>
+  const formReducer = (state, action) => {
+    switch (action.type) {
+      case "UPDATE_FIELD":
+        return {
+          ...state,
+          data: { ...state.data, [action.fieldName]: action.payload },
+          errors: {
+            ...state.errors,
+            [action.fieldName]:
+              action.payload === "" ? `${action.fieldName} required` : "",
+          },
+        };
+      // case "VALIDATE_FIELD":
+      //   return {
+      //     ...state,
+      //     errors: {
+      //       ...state.errors,
+      //       [action.fieldName]: action.payload ? "" : action.errMsg,
+      //     },
+      //     //   isValid: action.payload,
+      //     errorMessage: action.payload ? "" : action.errMsg,
+      //   };
+      case "VALIDATE":
+        const errors = {};
+        errors.comment = state.data.comment ? "" : "Comment required!";
+        errors.rate = state.data.rate ? "" : "Rate required!";
+        return {
+          ...state,
+          errors: errors,
+        };
+      default:
+        return state;
+    }
+  };
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Rate (1-5)</Form.Label>
-                <Form.Control type="number" min="1" max="5" placeholder="Rate" />
-            </Form.Group>
+  // Inizializzazione dello stato iniziale del form
+  const initialFormState = {
+    data: {
+      comment: "",
+      rate: "",
+    },
+    errors: {},
+    isValid: false,
+    errorMessage: "Enter data please",
+  };
 
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-    );
-}
+  const [formData, dispatch] = useReducer(formReducer, initialFormState);
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const newValue = value.toString();
 
+    // Aggiornamento dello stato con l'azione 'UPDATE_FIELD'
+    dispatch({
+      type: "UPDATE_FIELD",
+      fieldName: name,
+      payload: newValue,
+    });
+  };
+
+  // Funzione per validare il campo
+  // const handleValidation = (fieldName) => {
+  //   // Esegui la logica di validazione
+  //   let isValid = false;
+  //   let fieldValue = formData.data[fieldName];
+  //   let errMsg = "";
+  //   if (fieldValue !== "") isValid = true;
+  //   else {
+  //     errMsg = `Please enter ${fieldName}`;
+  //   }
+  //   if (fieldName === "rate")
+  //     if (parseInt(fieldValue) < 1 || parseInt(fieldValue) > 5)
+  //       errMsg = `${fieldName} must be between 1 and 5`;
+
+  //   // Aggiornamento dello stato con l'azione 'VALIDATE_FIELD'
+  //   dispatch({
+  //     type: "VALIDATE_FIELD",
+  //     fieldName: fieldName,
+  //     payload: isValid,
+  //     errMsg: isValid ? "" : `${fieldName}: inserisci`,
+  //   });
+  // };
+
+  // Funzione per gestire l'invio del form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: "VALIDATE" });
+    let hasError = false;
+    for (let key in formData.errors) {
+      if (formData.errors.hasOwnProperty(key)) {
+        // console.log(`${key}: ${formData.errors[key]}`);
+        if (formData.errors[key] !== "") {
+          alert(formData.errors[key]);
+          hasError = true;
+          break;
+        }
+      }
+    }
+    if (Object.keys(formData.errors).length < 2) {
+      alert("Complete the form");
+    } else if (!hasError) {
+      onAdd(formData.data);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h3 className="text-center">Add new comment</h3>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Comment</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter comment"
+          name="comment"
+          value={formData.data.comment}
+          onChange={handleChange}
+          // onBlur={() => handleValidation("comment")}
+        />
+        <Form.Text className="text-muted"></Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Rate (1-5)</Form.Label>
+        <Form.Control
+          type="number"
+          min="1"
+          max="5"
+          placeholder="Enter rate"
+          name="rate"
+          value={formData.data.rate}
+          onChange={handleChange}
+          // onBlur={() => handleValidation("rate")}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
 export default AddComment;
--- src/components/comments/CommentArea.jsx
+++ src/components/comments/CommentArea.jsx
@@ -1,60 +1,72 @@
-import { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import CommentList from "./CommentList";
-import LoadSpinner from '../loading/LoadSpinner';
-import AddComment from "./AddComment"
+import LoadSpinner from "../loading/LoadSpinner";
+import AddComment from "./AddComment";
 
 function CommentArea({ asin, onClose }) {
   const [show, setShow] = useState(true);
   const [comments, setComments] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [added, setAdded] = useState(0);
 
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmNzEzOGJkNWQxMjAwMTg5MGQzNGEiLCJpYXQiOjE3MDgwNzI4NDYsImV4cCI6MTcwOTI4MjQ0Nn0.1sxHpcQ5oKegWdxkGgevHAXbQexhncKJC4GyeRA3aq0";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmNzEzOGJkNWQxMjAwMTg5MGQzNGEiLCJpYXQiOjE3MDgwNzI4NDYsImV4cCI6MTcwOTI4MjQ0Nn0.1sxHpcQ5oKegWdxkGgevHAXbQexhncKJC4GyeRA3aq0";
   const options = {
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        `Bearer ${token}`
-    }
-  }
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
   const getComments = async () => {
     try {
       setLoading(true);
-      const resp = await fetch(`https://striveschool-api.herokuapp.com/api/books/${asin}/comments/`, options);
+      const resp = await fetch(
+        `https://striveschool-api.herokuapp.com/api/books/${asin}/comments/`,
+        options
+      );
       const data = await resp.json();
       setComments(data);
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       setError(error.message);
     }
   };
 
   const addComment = async (comment) => {
-    // try {
-    //   const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
-    //     ...options,
-    //     method: "POST",
-    //     body: JSON.stringify(comment)
-    //   },);
-    //   if(!response.ok){
-    //     setError(await response.text());
-    //   }
-    // } catch (e) {
-    //   setError(error.message);
-    // }
-  }
+    comment = {
+      ...comment,
+      elementId: asin,
+    };
+    try {
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/",
+        {
+          ...options,
+          method: "POST",
+          body: JSON.stringify(comment),
+        }
+      );
+      if (!response.ok) {
+        setError(await response.text());
+      }
+      setAdded(added + 1);
+    } catch (e) {
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     getComments();
-  }, [])
+  }, [added]);
 
   const handleClose = () => {
     setShow(false);
     onClose();
-  }
+  };
 
   return (
     <>
@@ -65,7 +77,7 @@ function CommentArea({ asin, onClose }) {
         <Modal.Body className="row justify-content-center mx-3">
           {error && <div>Ops, something went wrong...</div>}
           {loading && !error && <LoadSpinner />}
-          {!loading && !error && <AddComment onAdd={addComment}/>}
+          {!loading && !error && <AddComment onAdd={addComment} />}
           {!loading && !error && <CommentList comments={comments} />}
         </Modal.Body>
         <Modal.Footer>
